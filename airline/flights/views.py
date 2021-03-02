c3504e7542ecb1fb0d77141afff51cbdfb685455
timothyryeyn <timothyryeyn.perez@gmail.com>
2021-03-02 08:05:38 +0800

    Add list and adding of passenger in flight

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-from .models import Flight
+from django.urls import reverse
+
+from django.http import HttpResponseRedirect
+
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -11,6 +15,16 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
-        "flight":flight
+        "flight":flight,
+        "passengers":flight.passengers.all(),
+        "non_passengers":Passenger.objects.exclude(flights=flight).all()
     })
+
+def book(request, flight_id):
+    if request.method == 'POST':
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST['passenger']))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse('flight', args=(flight_id,)))
+
 
